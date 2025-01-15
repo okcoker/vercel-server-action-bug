c1a646e48adfea98ee6729f5bbce2aabb8b1e044
Sean Coker <1030830+okcoker@users.noreply.github.com>
2025-01-15 16:27:08 -0500

    My 12mb image was causing a body of 44mb

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@
 module.exports = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "50mb",
+      // The image doesnt have to be this large but gives some buffer
+      // for the rest of the post body
+      bodySizeLimit: "100mb",
     },
   },
 };
