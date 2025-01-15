0997c60b34087c2624272b2b29a823a2560196f8
Sean Coker <1030830+okcoker@users.noreply.github.com>
2025-01-15 16:19:25 -0500

    Remove destructure

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 import clsx from "clsx";
 import { uploadFile } from "./actions/uploadFile";
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+type Status = "idle" | "uploading" | "reading";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
   const [imageBytes, setImageBytes] = useState<ArrayBuffer | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [status, setStatus] = useState<Status>("idle");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("reading");
     const file = e.currentTarget.files?.[0];
     if (!file) {
       setError("No file found. try again");
@@ -17,17 +21,18 @@ export default function Home() {
     }
     const imageBytes = await readFileAsBytes(file);
     setImageBytes(imageBytes);
+    setStatus("idle");
   }
 
   async function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
-      setIsLoading(true);
+      setStatus("uploading");
       if (!imageBytes) {
         throw new Error("No file selected");
       }
 
-      const { message } = await uploadFile({
+      const result = await uploadFile({
         file: new Uint8Array(imageBytes),
       }).catch((err) => {
         // Throw a custom error here
@@ -35,7 +40,11 @@ export default function Home() {
         throw new Error(err.message);
       });
       setError(null);
-      setSuccessMessage(message);
+      setSuccessMessage(result.message);
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+      setImageBytes(null);
     } catch (err: unknown) {
       const message =
         err instanceof Error
@@ -45,11 +54,13 @@ export default function Home() {
       setError(message);
       setSuccessMessage(null);
     }
-    setIsLoading(false);
+    setStatus("idle");
   }
 
   let buttonText = "Upload";
-  if (isLoading) {
+  if (status === "reading") {
+    buttonText = "Reading file…";
+  } else if (status === "uploading") {
     buttonText = "Uploading…";
   } else if (imageBytes) {
     buttonText = `Upload ${imageBytes.byteLength} bytes`;
@@ -59,22 +70,31 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold">Vercel function bug</h1>
-        <h3 className="text-xl font-bold">Choose file</h3>
         <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
-          <input type="file" name="file" onChange={handleFileChange} />
+          <input
+            type="file"
+            name="file"
+            onChange={handleFileChange}
+            ref={inputRef}
+          />
           <button
             type="submit"
             disabled={imageBytes === null}
             className={clsx("rounded-md p-2", {
-              "bg-slate-400": !isLoading && imageBytes === null,
+              "bg-slate-400": !(status === "uploading") && imageBytes === null,
               "bg-green-800 hover:bg-green-700": imageBytes !== null,
-              "opacity-50": isLoading || imageBytes === null,
+              "opacity-50":
+                status === "uploading" ||
+                status === "reading" ||
+                imageBytes === null,
             })}
           >
             {buttonText}
           </button>
-          {error && <p>Error message: {error}</p>}
-          {successMessage && <p>Success message: {successMessage}</p>}
+          {error && <p className="text-red-500">Error: {error}</p>}
+          {successMessage && (
+            <p className="text-green-500">Success: {successMessage}</p>
+          )}
         </form>
       </main>
     </div>
