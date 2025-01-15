2dc868bc1e637cd5785453c15c358400ec5a6bbf
Sean Coker <1030830+okcoker@users.noreply.github.com>
2025-01-15 16:33:03 -0500

    Add github link

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,18 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl font-bold">Vercel function bug</h1>
+        <header>
+          <h1 className="text-2xl font-bold">Vercel function bug</h1>
+          <p>
+            Nothing is actually stored from your upload. See the{" "}
+            <a
+              className="text-blue-500"
+              href="https://github.com/okcoker/vercel-server-action-bug"
+            >
+              github repo
+            </a>
+          </p>
+        </header>
         <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
           <input
             type="file"
